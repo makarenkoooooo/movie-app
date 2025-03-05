4aff5badea9ccf1e26af271d43596b2482420122
makarenkoooooo <dodiverinwork@gmail.com>
2025-03-05 14:43:37 +0500

    add new file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { store } from './redux/store' // Подключаем store
+import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { store } from './redux/store'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+
+// Выбираем правильный Router в зависимости от среды
+const Router =
+  process.env.NODE_ENV === 'production' ? HashRouter : BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {' '}
-      {/* Оборачиваем приложение в Redux Provider */}
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 )
 
-// Если нужно измерять производительность
 reportWebVitals()
